dee84ab9e61e2bb2f10b454aa6fae544818c9ec3
james <james3895@duck.com>
2025-07-13 00:01:20 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://tetris-leaderboard-7nguxl5bqa-ue.a.run.app; img-src 'self' data:;">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://tetris-leaderboard.onrender.com; img-src 'self' data:;">
         <title>Tetris Game by James</title>
         <style>
             * {
